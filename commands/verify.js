b4be119b338a904674038c945adf3ddc837f6b03
Ramón Huidobro <hola@ramonh.dev>
2023-08-01 00:08:03 +0200

    Let's give this a try

--- commands/verify.js
+++ commands/verify.js
@@ -1,10 +1,28 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const channelID = process.env.CHANNEL_ID;
+const messageID = process.env.MESSAGE_ID;
+const roleID = process.env.ROLE_ID;
+const emojiID = process.env.EMOJI_ID;
 
 module.exports = {
-      data: new SlashCommandBuilder()
-          .setName('verify')
-          .setDescription('Replies with Pong!'),
-      async execute(interaction) {
-            await interaction.reply('Pong!');
-        },
-  };
+  data: new SlashCommandBuilder()
+    .setName("verify")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    const channel = await client.channels.fetch(channelID);
+    const message = await channel.messages.fetch(messageID);
+    const reaction = message.reactions.cache.get(emojiID);
+    const users = await reaction.users.fetch();
+
+    console.log(users);
+
+    // e.g. add a role to each user
+    users.each(async (user) => {
+      // get the member object as users don't have roles
+      const member = await message.guild.members.fetch(user.id);
+      //   member.roles.remove("ROLE ID");
+      await interaction.reply(user.name);
+    });
+    await interaction.reply("Pong!");
+  },
+};
