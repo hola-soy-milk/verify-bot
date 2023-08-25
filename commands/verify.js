d459ab0345269a08bb90439dd8dfb1474262f76d
Ramón Huidobro <hola@ramonh.dev>
2023-08-25 11:06:31 +0200

    Fix bot

--- commands/verify.js
+++ commands/verify.js
@@ -5,6 +5,26 @@ const roleID = process.env.ROLE_ID;
 const emojiID = process.env.EMOJI_ID;
 const serverID = process.env.SERVER_ID;
 
+const fetchAllUsers = async (reaction) => {
+  const users = [];
+  let last_id;
+
+  while(true) {
+    const options = {limit: 100};
+    if (last_id) {
+      options.after = last_id;
+    }
+    const newUsers = await reaction.users.fetch(options);
+    users.push(...Array.from(newUsers));
+    last_id = newUsers.last().id;
+
+    if (newUsers.size !== 100 || users >= 100) {
+      break;
+    }
+  }
+  return users.flat(Infinity);
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("verify")
@@ -15,12 +35,13 @@ module.exports = {
     const guild = await client.guilds.fetch(serverID);
     const reaction = message.reactions.cache.get(emojiID);
     const role = guild.roles.cache.get(roleID);
-    const users = await reaction.users.fetch();
 
-    console.log(`Ready to verify users in ${guild} ${role} with ${users}`);
+    const users = await fetchAllUsers(reaction);
+
+    console.log(`Ready to verify users in ${guild} ${role} with ${users.length}`);
 
     // e.g. add a role to each user
-    users.each(async (user) => {
+    users.forEach(async (user) => {
       // get the member object as users don't have roles
       try{
       const member = await guild.members.fetch(user.id);
@@ -28,7 +49,7 @@ module.exports = {
       if (member) {
         const role = member.roles.cache.find((r) => r.name === "unverified");
         if (role) {
-          console.log("verified");
+          console.log(user.displayName, "verified");
 
           member.roles.remove(role.id);
         }
